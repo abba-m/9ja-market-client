f1529ee5507a0382651c7e8d803fd5aaf6a1e061
Muhammad Mahmood <mmuhammadabba@gmail.com>
2022-02-04 02:05:23 +0100

    code refactor

--- src/components/myButton/index.js
+++
@@ -1,7 +0,0 @@
-import { Button } from "@chakra-ui/react";
-
-export default function MyButton({ buttonText, colorScheme, variant, size }){
-    return (
-        <Button variant={variant} colorScheme={colorScheme} size={size}>{buttonText}</Button>
-    )
-}
