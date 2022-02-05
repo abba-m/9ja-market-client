339bc9cc4d0adacf5a110248014d6f0f6b851042
Muhammad Mahmood <mmuhammadabba@gmail.com>
2022-02-05 21:50:29 +0100

    fix: removed code smell

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 
 import Dashboard from "pages/dashboardPage";
-import TestPage from "pages/testPage";
+import TestPage from "pages/testPage/testPage";
 
 //Nav Bar
-import NavBar from "components/appNavBar";
+import NavBar from "components/appNavBar/appNavBar";
 
 //user profile views
 import UserProfile from "pages/userProfile";
@@ -15,19 +16,20 @@ import PostsView from "pages/userProfile/views/posts";
 import ProfileView from "pages/userProfile/views/profile";
 import ReviewsView from "pages/userProfile/views/reviews";
 import SettingsView from "pages/userProfile/views/settings";
-import { Box } from "@chakra-ui/react";
 import NotFound from "components/404notFound";
+import PostAd from "pages/postAd/postAd";
 
 function App() {
   return (
-    <div className="App">
-      <Box>
-        <NavBar />
-      </Box>
-      <Box>
-        <Router>
+    <Router>
+      <Box className="App">
+        <Box w="100%">
+          <NavBar />
+        </Box>
+        <Box>
           <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/new-post" element={<PostAd />} />
             <Route path="/profile" element={<UserProfile />}>
               <Route index element={<ProfileView />} />
               <Route path="favorites" element={<FavoritesView />} />
@@ -40,9 +42,9 @@ function App() {
             <Route path="/test" element={<TestPage />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </Router>
+        </Box>
       </Box>
-    </div>
+    </Router>
   );
 }
 
