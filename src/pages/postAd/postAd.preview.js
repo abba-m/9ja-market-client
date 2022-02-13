5561f954eece154146bf89d7dd9efad283912da9
Muhammad Mahmood <mmuhammadabba@gmail.com>
2022-02-13 19:48:36 +0100

    completed postAd preview

--- src/pages/postAd/postAd.preview.js
+++ src/pages/postAd/postAd.preview.js
@@ -1,22 +1,19 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { useContext } from "react";
 import { PostAdContext } from "providers/postAdProvider";
+import { PRIMARY_COLOR } from "utils/constants";
 
 
 function PreviewAd() {
   
   const { handleForm: { getValues } } = useContext(PostAdContext);
-  const values = getValues()
-  console.log(Object.entries(values));
-
-
+  const values = getValues();
   
   return (
     <Box w="50" h="85%" overflowY="auto" px={4}>
-      <Text>Hello preview Component</Text> 
        {Object.entries(values).map(value => (
          <>
-         <Heading>{value[0]}</Heading>
+         <Heading size="md" color={PRIMARY_COLOR}>{value[0]}</Heading>
          <Text>{value[1]}</Text>
          </>
        ))}
