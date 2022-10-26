e44acf9a0785909a148a034f81fdae63a6566d0f
Muhammad Mahmood <mmuhammadabba@gmail.com>
2022-10-26 13:10:28 +0100

    fix: fix

--- src/pages/chatPage/chatPage.jsx
+++ src/pages/chatPage/chatPage.jsx
@@ -2,7 +2,6 @@ import ChatSideBar from "./chatSideBar";
 import { Outlet } from "react-router-dom";
 import { useMediaQuery } from "@chakra-ui/react";
 
-import "./styles/Chat.css";
 import "./styles/chat.style.css";
 
 const ChatPage = () => {
